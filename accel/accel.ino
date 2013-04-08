b94010951267b1298ea2dc590fa95fd06ee81d30
u-579 <u-579@hotmail.com>
2013-04-07 23:56:51 -0500

    Added gyro query

--- accel/accel.ino
+++ accel/accel.ino
@@ -18,7 +18,9 @@ int accelx();
 int accely();
 int accelz();
 
-int 
+int gyrox();
+int gyroy();
+int gyroz();
 
 void setup()
 {
@@ -93,48 +95,48 @@ void initHardware()
 
 int accelx()
 {
-  uint8_t accelx_H = regRead(0x3B, chip_select);
-  uint8_t accelx_L = regRead(0x3C, chip_select);
+  uint8_t accelx_H = regRead(0x3B);
+  uint8_t accelx_L = regRead(0x3C);
   int16_t accelx = accelx_H << 8 | accelx_L;
   return(accelx);
 }
 
 int accely()
 {
-  uint8_t accely_H = regRead(0x3D, chip_select);
-  uint8_t accely_L = regRead(0x3E, chip_select);
+  uint8_t accely_H = regRead(0x3D);
+  uint8_t accely_L = regRead(0x3E);
   int16_t accely = accely_H << 8 | accely_L;
   return(accely);
 }
 
 int accelz()
 {
-  uint8_t accelz_H = regRead(0x3F, chip_select);
-  uint8_t accelz_L = regRead(0x40, chip_select);
+  uint8_t accelz_H = regRead(0x3F);
+  uint8_t accelz_L = regRead(0x40);
   int16_t accelz = accelz_H << 8 | accelz_L;
   return(accelz);
 }
-/*
-int GyroX(int ChipSelPin)
+
+int gyrox()
 {
-  uint8_t GyroX_H=SPIread(0x43,ChipSelPin);
-  uint8_t GyroX_L=SPIread(0x44,ChipSelPin);
-  int16_t GyroX=GyroX_H<<8|GyroX_L;
-  return(GyroX);
+  uint8_t gyrox_H = regRead(0x43);
+  uint8_t gyrox_L = regRead(0x44);
+  int16_t gyrox = gyrox_H << 8 | gyrox_L;
+  return gyrox;
 }
 
-int GyroY(int ChipSelPin)
+int gyroy()
 {
-  uint8_t GyroY_H=SPIread(0x45,ChipSelPin);
-  uint8_t GyroY_L=SPIread(0x46,ChipSelPin);
-  int16_t GyroY=GyroY_H<<8|GyroY_L;
-  return(GyroY);
+  uint8_t gyroy_H = regRead(0x45);
+  uint8_t gyroy_L = regRead(0x46);
+  int16_t gyroy = gyroy_H << 8 | gyroy_L;
+  return gyroy;
 }
 
-int GyroZ(int ChipSelPin)
+int gyroz()
 {
-  uint8_t GyroZ_H=SPIread(0x47,ChipSelPin);
-  uint8_t GyroZ_L=SPIread(0x48,ChipSelPin);
-  int16_t GyroZ=GyroZ_H<<8|GyroZ_L;
-  return(GyroZ);
-}*/
+  uint8_t gyroz_H = regRead(0x47);
+  uint8_t gyroz_L = regRead(0x48);
+  int16_t gyroz = gyroz_H << 8 | gyroz_L;
+  return gyroz;
+}
